7cc9e315ea967f9105dcd2246e6a7ff2e8580334
sunzh <sunzh@huntor.cn>
2018-03-21 13:26:54 +0800

    backup

--- app.js
+++ app.js
@@ -12,159 +12,158 @@ var express = require( 'express' ),
     app = express();
 var appEnv = cfenv.getAppEnv();
 
-console.log('12312')
+const HyperledgerClient = require( './hyperledger-client' );
+const client = new HyperledgerClient();
 
-// const HyperledgerClient = require( './hyperledger-client' );
-// const client = new HyperledgerClient();
+app.use( multer( { dest: './tmp/' } ).single( 'file' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+app.use( express.static( __dirname + '/public' ) );
 
-// app.use( multer( { dest: './tmp/' } ).single( 'file' ) );
-// app.use( bodyParser.urlencoded( { extended: true } ) );
-// app.use( bodyParser.json() );
-// app.use( express.static( __dirname + '/public' ) );
-
-// app.get( '/', function( req, res ){
-//   var template = fs.readFileSync( __dirname + '/public/index.ejs', 'utf-8' );
-//   res.write( ejs.render( template, {} ) );
-//   res.end();
-// });
+app.get( '/', function( req, res ){
+  var template = fs.readFileSync( __dirname + '/public/index.ejs', 'utf-8' );
+  res.write( ejs.render( template, {} ) );
+  res.end();
+});
 
 
-// app.get( '/participants', function( req, res ){
-//   client.getAllParticipants( result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+app.get( '/participants', function( req, res ){
+  client.getAllParticipants( result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.get( '/participant', function( req, res ){
-//   var participantId = req.query.participantId;
+app.get( '/participant', function( req, res ){
+  var participantId = req.query.participantId;
 
-//   client.getParticipant( participantId, result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.getParticipant( participantId, result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.post( '/participant', function( req, res ){
-//   var participantId = req.body.participantId;
-//   var firstName = req.body.firstName;
-//   var lastName = req.body.lastName;
-//   var companyName = req.body.companyName;
-//   var participant = { participantId: participantId, firstName: firstName, lastName: lastName, companyName: companyName };
+app.post( '/participant', function( req, res ){
+  var participantId = req.body.participantId;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var companyName = req.body.companyName;
+  var participant = { participantId: participantId, firstName: firstName, lastName: lastName, companyName: companyName };
 
-//   client.updateParticipantTx( participant, result => {
-//     res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.updateParticipantTx( participant, result => {
+    res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.delete( '/participant', function( req, res ){
-//   var participantId = req.body.participantId;
+app.delete( '/participant', function( req, res ){
+  var participantId = req.body.participantId;
 
-//   client.deleteParticipantTx( participantId, result => {
-//     res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.deleteParticipantTx( participantId, result => {
+    res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.get( '/assets', function( req, res ){
-//   client.getAllAssets( result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+app.get( '/assets', function( req, res ){
+  client.getAllAssets( result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.get( '/asset', function( req, res ){
-//   var assetId = req.query.assetId;
+app.get( '/asset', function( req, res ){
+  var assetId = req.query.assetId;
 
-//   client.getAsset( assetId, result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.getAsset( assetId, result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.post( '/asset', function( req, res ){
-//   var assetId = req.body.assetId;
-//   var participantId = req.body.participantId;
-//   var name = req.body.name;
-//   var created = req.body.created;
-//   var price = req.body.price;
-//   //. K.Kimura(2017/Aug/18) How to specify owner??
-//   var asset = { assetId: assetId, owner: "resource:me.juge.sample.MyParticipant#" + participantId, name: name, created: created, price: price };
+app.post( '/asset', function( req, res ){
+  var assetId = req.body.assetId;
+  var participantId = req.body.participantId;
+  var name = req.body.name;
+  var created = req.body.created;
+  var price = req.body.price;
+  //. K.Kimura(2017/Aug/18) How to specify owner??
+  var asset = { assetId: assetId, owner: "resource:me.juge.sample.MyParticipant#" + participantId, name: name, created: created, price: price };
 
-//   client.updateAssetTx( asset, result => {
-//     res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.updateAssetTx( asset, result => {
+    res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.delete( '/asset', function( req, res ){
-//   var assetId = req.body.assetId;
+app.delete( '/asset', function( req, res ){
+  var assetId = req.body.assetId;
 
-//   client.deleteAssetTx( assetId, result => {
-//     res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.deleteAssetTx( assetId, result => {
+    res.write( JSON.stringify( { status: 'ok' }, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// app.get( '/queryAssetsByPrice', function( req, res ){
-//   var priceFrom = parseInt( req.query.priceFrom );
-//   var priceTo = parseInt( req.query.priceTo );
-//   var condition = { priceFrom: priceFrom, priceTo: priceTo };
+app.get( '/queryAssetsByPrice', function( req, res ){
+  var priceFrom = parseInt( req.query.priceFrom );
+  var priceTo = parseInt( req.query.priceTo );
+  var condition = { priceFrom: priceFrom, priceTo: priceTo };
 
-//   client.queryAssetsByPrice( condition, result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
+  client.queryAssetsByPrice( condition, result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
 
-// /*
-//  * Can not query with Relationships
-//  *
-// app.get( '/queryAssetsByCompanyName', function( req, res ){
-//   var companyName = req.query.companyName;
-//   var condition = { companyName: companyName };
+/*
+ * Can not query with Relationships
+ *
+app.get( '/queryAssetsByCompanyName', function( req, res ){
+  var companyName = req.query.companyName;
+  var condition = { companyName: companyName };
 
-//   client.queryAssetsByCompanyName( condition, result => {
-//     res.write( JSON.stringify( result, 2, null ) );
-//     res.end();
-//   }, error => {
-//     res.write( JSON.stringify( error, 2, null ) );
-//     res.end();
-//   });
-// });
-// */
+  client.queryAssetsByCompanyName( condition, result => {
+    res.write( JSON.stringify( result, 2, null ) );
+    res.end();
+  }, error => {
+    res.write( JSON.stringify( error, 2, null ) );
+    res.end();
+  });
+});
+*/
 
 
-var port = appEnv.port || 3002;
-app.listen(13002);
+// var port = appEnv.port || 3002;
+var port = 13002;
+app.listen( port );
 console.log( "server starting on " + port + " ..." );
 
 
